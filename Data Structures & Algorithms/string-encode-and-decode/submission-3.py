e63105f50ccf4f5756b96bbfd7cfd3cc1ec26894
Abereni Opuiyo <105760232+abereni4u@users.noreply.github.com>
2026-05-19 13:51:48 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return "".join(f"{len(s)}#{s}" for s in strs)
+
+
+    def decode(self, s: str) -> List[str]:
+        # Get the length of each string before the #
+        # Instead of using slicing to create a new string everytime which is an O(n) operation
+        # --use a pointer instead
+
+        j = 0
+        decoded = []
+        while(j < len(s)):
+            boundary = s.index("#", j)
+            length = int(s[j:boundary])
+            dWord = s[boundary + 1: boundary + 1 + length]
+            decoded.append(dWord)
+            j = boundary + 1 + length
+
+        return decoded
